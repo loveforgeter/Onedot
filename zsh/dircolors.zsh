6b6912447a040a237490e56533802b4c218e2ff2
loveforgeter <loveforgeter@gmail.com>
2015-07-09 16:15:13 +0800

    update dircolors

--- zsh/dircolors.zsh
+++ zsh/dircolors.zsh
@@ -3,7 +3,7 @@ PWD="$( cd "$(dirname $0)" && pwd )"
 export TERM=xterm-256color
 if [[ $(uname -s) == "Darwin" ]];then
   if $(hash brew) && $(brew list | grep -q coreutils);then
-    eval $(gdircolors $PWD/dircolors/dircolors-solarized/dircolors.256dark)
+    eval $(gdircolors $PWD/dircolors/dircolors.ansi-dark)
     alias ls='gls --color=auto'
     zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
   else
@@ -12,7 +12,7 @@ if [[ $(uname -s) == "Darwin" ]];then
     alias ls='ls -G'
   fi
 else
-  eval $(dircolors $PWD/dircolors/dircolors-solarized/dircolors.256dark)
+  eval $(dircolors $PWD/dircolors/dircolors.ansi-dark)
   alias ls='ls --color=auto'
   zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 fi
